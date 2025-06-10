4a6a2182af72e879ff45f0ac184aab436e1fb2a6
Chaehwan Lim <legoch16@gmail.com>
2025-06-10 21:13:40 +0900

    refactor : 중간지점 탐색 알고리즘 최적화
    - 역 두개를 입력받은 경우에는 N-1 계산하지 않고, 무조건 두 역 사용하도록 설정
    - N-1 선정 기준 강화
      - N개 기준으로 했을 때보다 시간 편차 및 절대적인 시간 모두 줄어들 경우

--- src/main/java/net/skhu/dto/StationMidpointResponse.java
+++ src/main/java/net/skhu/dto/StationMidpointResponse.java
@@ -11,9 +11,16 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class StationMidpointResponse {
-    private List<String> sources;              // 다수의 출발역 리스트
+    private List<String> sources;              			// 다수의 출발역 리스트
     @JsonProperty("midpoint")
-    private String balancedMidpoint;           // 최적 중간역 이름
-    private Map<String, Double> timeFromSourceToMid; // 출발역별 중간역까지 소요시간 맵
-    private double timeDifference;             // 최대/최소 소요시간 차이
+    private String balancedMidpoint;           		 	// 최적 중간역 이름
+    private Map<String, Double> timeFromSourceToMid; 	// 출발역별 중간역까지 소요시간 맵
+    private double timeDifference;            			// 최대/최소 소요시간 차이    
+    
+    // 최고 소요시간을 계산해 반환
+    public double getMaxTime() {
+        return timeFromSourceToMid.values().stream()
+                .max(Double::compare)
+                .orElse(Double.POSITIVE_INFINITY);
+    }
 }
